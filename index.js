eef4fa5b02466669ec4e6df42a992441939ccb66
Rabah Abellache <agar3arab@gmail.com>
2023-07-03 18:18:19 +0100

    database connection

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const app = express();
-app.use(express.json());
 
+
+
+const app = express();
+const port = 3000;
+app.use(express.json());
 const uri = process.env.URI;
+const apiRoutes = require('./api-routes');
+app.use('/api', apiRoutes);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -15,17 +21,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
+const connect = async ()=> {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  }catch (error){
+    console.log(error)
+  }finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+
+
+connect().then(() => {
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+      });
+      
+})
+
+
+
+
 
