063dfaa8a9f4071599050255ba1ed8c0b9c647bd
Rabah Abellache <agar3arab@gmail.com>
2023-07-04 15:00:03 +0100

    worker model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { path } = require('express/lib/application');
 const mongoose = require('mongoose');
+const { double } = require('webidl-conversions');
 
 
 const User = mongoose.model("user", new mongoose.Schema({
@@ -47,7 +48,40 @@ const User = mongoose.model("user", new mongoose.Schema({
     comments : {
         type: [String],
         required: false
+    },
+    latitude : {
+        type: double,
+        required: false
+    },
+    longitude : {
+        type: double,
+        required: false
+    },
+    cratedAt : {
+        type: Date,
+        default: Date.now
+    },
+    work : {	
+        type: String,
+        required: true
+    },
+    bio : {
+        type: String,
+        required: true
+    },
+    rating : {  
+        type: Number,
+        default: 0
+    },
+    workTime : {
+        type: String,
+        required: true
+    },
+    photos : {
+        type: [String],
+        required: false
     }
+
 }))
 
 
