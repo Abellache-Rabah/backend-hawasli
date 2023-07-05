5e7c47253d809bb3e122041ed7df3b03ed83b743
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-07-05 16:02:49 +0100

    commit

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 const jwtverify = (req, res, next) => {
   const authHead = req.headers?.authorization;
   const token = authHead?.split(" ")[1];
-  if (!token) return res.render("index");
+  if (!token) return res.status(401).json({err:"token dont exsist!"});
   jwt.verify(token, process.env.K, (err, decode) => {
-    if (err) return res.render("index");
+    if (err) return res.status(401).json({err})
     if (!decode) {
-      return res.status(401).render("index");
+      return res.status(401).json({err:"token is expire!"})
     }
-    req.user = decode.email;
+    req.email = decode.email;
     next();
   });
 };
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,39 +1,56 @@
 const Router = require("express").Router();
 const multer = require("multer");
 const path = require("path");
-
+const { jwtverify } = require("../middlewares/jwt");
+const { WorkerModel } = require("../models/workerModel");
+const { ConsummerModel } = require("../models/consummerModel");
+const fs = require("fs");
 const storage = multer.diskStorage({
-    destination: "./uploads/profile",
-    filename : (req , file , cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
-    }
-})
+  destination: async function (req, file, cb) {
+    const email = req?.email;
+    const worker = await WorkerModel.findOne({ email });
+    const consummer = await ConsummerModel.findOne({ email });
+    const id = consummer?._id || worker?._id;
+    let path = `uploads/pictures/${id}`;
+    fs.mkdirSync(path, { recursive: true })
+    cb(null, path);
+  },
+  filename: async (req, file, cb) => {
+    const email = req?.email;
+    const worker = await WorkerModel.findOne({ email });
+    const consummer = await ConsummerModel.findOne({ email });
+    const id = consummer?._id || worker?._id;
+    return cb(null, `profile${path.extname(file.originalname)}`);
+  },
+});
 
 function errHandler(err, req, res, next) {
-    if (err instanceof multer.MulterError) {
-        res.json({
-            success: 0,
-            message: err.message
-        })
-    }
+  if (err instanceof multer.MulterError) {
+    res.json({
+      success: 0,
+      message: err.message,
+    });
+  }
 }
 
-
 const upload = multer({
-    storage: storage ,
-    limits: {
-        fileSize: 100000
-    }
-})
+  storage: storage,
+  limits: {
+    fileSize: 10000000,
+  },
+});
 
-
-Router.post("/uploadProfile",upload.single("profile"), (req, res) => {
+Router.post(
+  "/uploadProfile",
+  jwtverify,
+  upload.single("profile"),
+  (req, res) => {
     res.json({
-            status : 200,
-            message: "Profile Uploaded Successfully",
-            url:`http://localhost:3000/uploads/profile/${req.file.filename}`
-        })
-})
+      status: 200,
+      message: "Profile Uploaded Successfully",
+      url: `http://localhost:3000/uploads/profile/${req.file.filename}`,
+    });
+  }
+);
 
-
-module.exports = {Router,errHandler};
+module.exports = { Router, errHandler };
