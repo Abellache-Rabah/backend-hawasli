7ad45fb2a5a14c3ec18ee32059a977ca1258c69d
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-07-06 17:25:11 +0100

    fix upgrade to worker

--- routes/Authentication/auth.js
+++ routes/Authentication/auth.js
@@ -5,18 +5,8 @@ const { ConsummerModel } = require("../../models/consummerModel");
 const { TokenModel } = require("../../models/tokenModels");
 const bcrypt = require("bcrypt");
 const { jwtverify, isConsummer } = require("../../middlewares/jwt");
-const {hashPassword, comparePassword} = require("../../utils/password")
-
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-      user: process.env.USER,
-      pass: process.env.PASS
-  }
-});
-
-
+const { hashPassword, comparePassword } = require("../../utils/password");
+const nodemailer = require("nodemailer");
 
 const createAccessToken = (email) => {
   return jwt.sign({ email }, process.env.K);
@@ -34,7 +24,7 @@ Roater.post("/login", async (req, res) => {
   const consummer = await ConsummerModel.findOne({
     email,
   });
-  if(!worker&&!consummer) return res.status(401).send("email dont exsist!")
+  if (!worker && !consummer) return res.status(401).send("email dont exsist!");
   const isAuth = comparePassword(
     password,
     worker?.password || consummer?.password
@@ -57,98 +47,55 @@ Roater.post("/register", async (req, res) => {
   const consummer = await ConsummerModel.findOne({ email });
   if (consummer) return res.status(401).send({ message: "user already exist" });
 
-const user = req.body;
-  const verfyToken = jwt.sign({
-    user ,
-}, process.env.K, { expiresIn: '10m' }  
-);    
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
+  const user = req.body;
+  const verfyToken = jwt.sign(
+    {
+      user,
+    },
+    process.env.K,
+    { expiresIn: "10m" }
+  );
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
       user: process.env.USER,
-      pass: process.env.PASS
-  }
-});
+      pass: process.env.PASS,
+    },
+  });
   const mailConfigurations = {
     // It should be a string of sender/server email
     from: process.env.USER,
     to: email,
     // Subject of Email
-    subject: 'Email Verification',
+    subject: "Email Verification",
     // This would be the text of email body
     text: `Hi! There, You have recently visited 
            our website and entered your email.
            Please follow the given link to verify your email
            http://localhost:3000/verify/${verfyToken} 
-           Thanks`    
-};
-transporter.sendMail(mailConfigurations, function(error, info){
+           Thanks`,
+  };
+  transporter.sendMail(mailConfigurations, function (error, info) {
     if (error) throw Error(error);
-    res.status(200).send('Email Sent Successfully');
+    res.status(200).send("Email Sent Successfully");
     console.log(info);
-});
-
-  
+  });
 });
 
 Roater.post("/upgradeToWorker", jwtverify, isConsummer, async (req, res) => {
-  const { sex, work, phone, age, wilaya, baladia, latitude, longitude } = req.body;
-  if (
-    !baladia ||
-    !sex ||
-    !work ||
-    !phone ||
-    !age ||
-    !wilaya ||
-    !latitude || 
-    !longitude
-  )
-    return res
-      .status(401)
-      .send({ message: "error all fields should be present " });
+  const { sex, work, phone, age, wilaya, baladia, latitude, longitude } =
+    req.body;
   const email = req?.email;
-  const consummer = ConsummerModel.findOne({ email });
-  if (!consummer) {
-    return res.status(401).send({ message: "user does not exist" });
-  } else {
-    const worker = await WorkerModel.findOne({ email });
-    if (worker) {
-      return res.status(401).send({ message: "you are allready a worker" });
-    } else {
-      
-
-    }
-  }
-
-  if (
-    !firstName ||
-    !lastName ||
-    !email ||
-    !password ||
-    !baladia ||
-    !sex ||
-    !work ||
-    !phone ||
-    !age ||
-    !wilaya
-  )
+  if (!baladia || !sex || !work || !phone || !age || !wilaya)
     return res
       .status(401)
       .send({ message: "error all fields should be present " });
   const worker = await WorkerModel.findOne({ email });
   if (worker) return res.status(401).send({ message: "user already exist" });
-  const hashPwd = await hashPassword(password);
+  const consummer = ConsummerModel.findOneAndDelete({ email }, { new: true });
   const newWorker = new WorkerModel({
-    firstName,
-    lastName,
-    email,
-    password: hashPwd,
-    sex,
-    work,
-    phone,
-    age,
-    wilaya,
-    baladia,
+    ...consummer,
+    ...req.body,
   });
   await newWorker.save();
   const token = createAccessToken(email);
